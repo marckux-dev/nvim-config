ba4059eb202a5e8592ba9c15ef1c5ab4ce3ffbca
marckux-dev <marckux.developer@gmail.com>
2025-10-15 01:45:33 +0200

    Completion java ok, Not Lombok Yet

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -41,7 +41,6 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-			local lspconfig = require("lspconfig")
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			vim.lsp.enable("jdtls")
 			vim.lsp.config("jdtls", {
