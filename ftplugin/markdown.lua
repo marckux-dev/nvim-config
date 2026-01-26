2e8364384d15c9017dc11489dd1587fe96efb1c2
marckux-dev <marckux.developer@gmail.com>
2026-01-26 12:29:15 +0100

    spell para markdown

---
+++ ftplugin/markdown.lua
@@ -0,0 +1,10 @@
+-- path: /home/marckux/.config/nvim/ftplugin/markdown.lua
+
+-- Activar el corrector ortográfico
+vim.opt_local.spell = true
+
+-- Configurar el idioma a español
+vim.opt_local.spelllang = "es"
+
+-- (Opcional) Si tienes un archivo de texto con palabras personalizadas:
+-- vim.opt_local.dictionary:append("/usr/share/dict/spanish")
